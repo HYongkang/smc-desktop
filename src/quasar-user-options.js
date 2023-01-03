526bae2452dff3332dfde46ba9770f5c5c8eceed
HYongkang <hyk8899@mail.notes.bank-of-china.com>
2023-01-03 09:20:55 +0800

    提交代码：vue+quasar

---
+++ src/quasar-user-options.js
@@ -0,0 +1,14 @@
+
+import './styles/quasar.scss'
+import lang from 'quasar/lang/zh-CN.js'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import '@quasar/extras/mdi-v4/mdi-v4.css'
+
+// To be used on app.use(Quasar, { ... })
+export default {
+  config: {},
+  plugins: {
+  },
+  lang: lang
+}
